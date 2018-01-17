3c4e986392e8bb8347b67e457af094bac931b643
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-01-17 18:09:52 +0200

    Added title length Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
+  validates :title, length: { minimum: 10,
+                              too_short: 'Title should be at least %{count} characters' }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
