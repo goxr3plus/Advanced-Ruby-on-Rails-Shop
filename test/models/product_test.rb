3453a7d45da0fc04c4fae0b48c279c544fe88040
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-01-17 18:03:05 +0200

    'Validation'

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -46,4 +46,14 @@ class ProductTest < ActiveSupport::TestCase
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
   end
+
+  test 'product is not valid without a unique title - i18n' do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'yyy',
+                          price: 1,
+                          image_url: 'fred.gif')
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                 product.errors[:title]
+  end
 end
